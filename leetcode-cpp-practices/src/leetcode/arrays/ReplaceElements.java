1c55f087d452b8d7d237928454c79cb0eba06e18
HarvindarRaghav <harvindarraghav1@gmail.com>
2023-05-21 18:40:29 +0530

    Replace Elements with Greatest Element on Right Side

--- leetcode-cpp-practices/src/leetcode/arrays/ReplaceElements.java
+++ leetcode-cpp-practices/src/leetcode/arrays/ReplaceElements.java
@@ -1,14 +1,27 @@
 package leetcode.arrays;
 
+import java.util.Arrays;
+
 public class ReplaceElements {
 
-	public ReplaceElements() {
-		
-	}
+	public int[] replaceElements(int[] arr) {
+        int n =arr.length;
+        int max = arr[n-1];
+        arr[n-1] = -1;
+        for(int i=n-2;i>=0;i--){
+            int temp = arr[i];
+            arr[i]=max;
+            if(max<temp)
+                max= temp;
+        }
+        return arr;
+    }
 
 	public static void main(String[] args) {
-		
-
+		int arr[]= {17,18,5,4,6,1};
+		ReplaceElements replaceElements =new ReplaceElements();
+		replaceElements.replaceElements(arr);
+		System.out.println(Arrays.toString(arr));
 	}
 
 }
